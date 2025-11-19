4c83842c9a3af7969a6fc8ce6caa2c824fb238f1
Ensar Sarajčić <dev@ensarsarajcic.com>
2025-11-19 16:49:57 +0100

    Add `data_block` tests for bit ranges split over more bytes

--- src/data_block.rs
+++ src/data_block.rs
@@ -52,7 +52,11 @@ impl From<u32> for Fingerprint {
 #[derive(Clone, Debug)]
 pub(crate) struct DataBlockFieldConfiguration {
     bytes: RangeInclusive<usize>,
-    mask: u32,
+    // Mask has to be u64, because even though max value can fit in u32, it can spread over 4 bytes
+    // due to layout
+    mask: u64,
+    shift: usize,
+    in_value_mask: u32,
 }
 
 impl DataBlockFieldConfiguration {
@@ -60,18 +64,21 @@ impl DataBlockFieldConfiguration {
         assert!(bits.len() <= 32);
         let start_byte = bits.start / 8; // Round down to take the lower byte
         let end_byte = (bits.end - 1) / 8;
-        let bytes = start_byte..=end_byte;
+        let bytes = dbg!(start_byte..=end_byte);
+        let shift = (end_byte + 1) * 8 - bits.end;
         Self {
             bytes,
+            shift,
+            mask: ((1u64 << bits.len()) - 1) << shift,
             // u64 is used to prevent overflow when bits.len() == 32
             // The final value will be at most u32::MAX, since bits.len() is limited to be <= 32
             #[allow(clippy::cast_possible_truncation)]
-            mask: ((1u64 << bits.len()) - 1) as u32,
+            in_value_mask: ((1u64 << bits.len()) - 1) as u32,
         }
     }
 
     pub(crate) fn value_mask(&self) -> u32 {
-        self.mask
+        self.in_value_mask
     }
 }
 
@@ -111,27 +118,29 @@ impl<'a> DataBlock<'a> {
         self.0
     }
 
+    #[allow(clippy::cast_possible_truncation)]
     pub(crate) fn load_bits(&self, config: &DataBlockFieldConfiguration) -> u32 {
         let loaded = &self.0[config.bytes.clone()];
-        let mut loaded_u32 = 0;
+        let mut loaded_u64 = 0;
         let len = loaded.len();
         for (i, b) in loaded.iter().enumerate() {
-            loaded_u32 += (*b as u32) << ((len - (i + 1)) * 8)
+            loaded_u64 += (*b as u64) << ((len - (i + 1)) * 8)
         }
-        loaded_u32 & config.mask
+        ((loaded_u64 & config.mask) >> config.shift) as u32
     }
 
     pub(crate) fn store_bits(&mut self, config: &DataBlockFieldConfiguration, value: u32) {
-        let masked_new_value = value & config.mask;
+        let masked_new_value = value & config.in_value_mask;
         let loaded = &self.0[config.bytes.clone()];
         let len = loaded.len();
-        let mut loaded_u32 = 0;
+        let mut loaded_u64 = 0;
         for (i, b) in loaded.iter().enumerate() {
-            loaded_u32 += (*b as u32) << ((len - (i + 1)) * 8)
+            loaded_u64 += (*b as u64) << ((len - (i + 1)) * 8)
         }
-        let masked_old_value = loaded_u32 & config.mask;
-        let final_value = masked_old_value | masked_new_value;
-        self.0[config.bytes.clone()].copy_from_slice(&final_value.to_be_bytes()[(4 - len)..]);
+        #[allow(clippy::cast_possible_truncation)]
+        let masked_old_value = loaded_u64 & !config.mask;
+        let final_value = masked_old_value | ((masked_new_value as u64) << config.shift);
+        self.0[config.bytes.clone()].copy_from_slice(&final_value.to_be_bytes()[(8 - len)..]);
     }
 
     pub(crate) fn get_fingerprint(&self, configuration: &CuckooConfiguration) -> Fingerprint {
@@ -221,14 +230,15 @@ impl<'a> DataBlock<'a> {
 }
 
 impl<'a> ReadOnlyDataBlock<'a> {
+    #[allow(clippy::cast_possible_truncation)]
     pub(crate) fn load_bits(&self, config: &DataBlockFieldConfiguration) -> u32 {
         let loaded = &self.0[config.bytes.clone()];
-        let mut loaded_u32 = 0;
+        let mut loaded_u64 = 0;
         let len = loaded.len();
         for (i, b) in loaded.iter().enumerate() {
-            loaded_u32 += (*b as u32) << ((len - (i + 1)) * 8)
+            loaded_u64 += (*b as u64) << ((len - (i + 1)) * 8)
         }
-        loaded_u32 & config.mask
+        ((loaded_u64 & config.mask) >> config.shift) as u32
     }
 
     pub(crate) fn inner(self) -> &'a [u8] {
@@ -270,7 +280,7 @@ mod tests {
         let mut data = [0u8; 4];
         let mut data_block = DataBlock::from(&mut data[..]);
         for i in 1usize..=32usize {
-            let field_config = DataBlockFieldConfiguration::new(0..i);
+            let field_config = dbg!(DataBlockFieldConfiguration::new(0..i));
             // Ensure we are using the max value possible for set bit count
             let value: u32 = ((1u64 << i) - 1).try_into().unwrap();
             data_block.reset();
@@ -281,5 +291,33 @@ mod tests {
                 "Loaded value was different for bit count {i}"
             );
         }
+    }
+
+    #[test]
+    fn test_load_store_at_non_byte_boundary() {
+        // Let's assume we have 13 bit fingerprint and additional data after it
+        let data_start = 13;
+        let fp_config = DataBlockFieldConfiguration::new(0..data_start);
+        let fp_value = 0b1010101010101u32;
+        let mut data = [0u8; 7];
+        let mut data_block = DataBlock::from(&mut data[..]);
+        data_block.store_bits(&fp_config, fp_value);
+        for i in 1usize..=32usize {
+            let field_config = dbg!(DataBlockFieldConfiguration::new(data_start..data_start + i));
+            // Ensure we are using the max value possible for set bit count
+            let value: u32 = ((1u64 << i) - 1).try_into().unwrap();
+            data_block.store_bits(&field_config, 0);
+            data_block.store_bits(&field_config, value);
+            assert_eq!(
+                data_block.load_bits(&field_config),
+                value,
+                "Loaded value was different for bit count {i}"
+            );
+        }
+        assert_eq!(
+            data_block.load_bits(&fp_config),
+            fp_value,
+            "Loads/stores wrote outside of their bits"
+        );
     }
 }
